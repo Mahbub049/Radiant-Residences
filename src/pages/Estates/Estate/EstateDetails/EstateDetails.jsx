5f423e5b3a72576e4ee44aa3d355b391bb8d5040
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-04-12 17:50:55 +0600

    mobile version estate details added

--- src/pages/Estates/Estate/EstateDetails/EstateDetails.jsx
+++ src/pages/Estates/Estate/EstateDetails/EstateDetails.jsx
@@ -21,25 +21,25 @@ const EstateDetails = () => {
         <div className="container mx-auto">
             <Navbar></Navbar>
             <div className="mt-16 gap-10">
-                <div className=" relative mb-12
+                <div className=" mx-3 lg:mx-0 relative mb-12
                 ">
-                    <div className={`absolute left-3 top-3 ${status === 'rent' ? 'bg-[#389b2f]' : 'bg-[#FF5A3D]'} px-2 py-1 text-white uppercase`}>For {status}</div>
+                    <div className={`absolute left-3 top-3 ${status === 'rent' ? 'bg-[#389b2f]' : 'bg-[#FF5A3D]'} px-2 mx-3py-1 text-white uppercase`}>For {status}</div>
                     <img className="w-full h-[700px] object-cover flex-1 rounded-lg" src={image} alt="" />
                 </div>
-                <div className="flex justify-between gap-32">
+                <div className="flex lg:flex-row flex-col justify-between gap-3 lg:gap-32 mx-3">
                     <div className="flex-1">
-                        <h2 className="text-4xl font-bold ">{estate_title}</h2>
+                        <h2 className="text-3xl lg:text-4xl font-bold ">{estate_title}</h2>
                         <div className=" flex gap-2 items-center my-4 text-orange-600 rounded-lg">
                             <IoLocationSharp className="text-xl" />
                             <p className="font-semibold">{location}</p>
                         </div>
-                        <h2 className="text-3xl font-bold text-[#FF5A3D]">{price}</h2>
+                        <h2 className="text-2xl lg:text-3xl font-bold text-[#FF5A3D]">{price}</h2>
                         <hr className="my-6" />
                         <p className="text-gray-500"><span className="font-semibold text-black">Description: </span>{description}</p>
                     </div>
                     <div className="flex-1">
                         <h4 className="mt-6 mb-4 font-semibold">Facilities:</h4>
-                        <div className="flex gap-6 font-semibold">
+                        <div className="flex lg:flex-row flex-col gap-6 font-semibold">
                         {
                             facilities.map((facility, idx)=><p key={idx} className="text-white bg-[#FF5A3D] p-2 rounded-lg">{facility}</p>)
                         }
@@ -47,7 +47,7 @@ const EstateDetails = () => {
                         <hr className="my-8" />
                         <div>
                             <p className="font-bold ">Estate Details:</p>
-                            <div className="flex gap-8 mt-4">
+                            <div className="flex lg:flex-row flex-col gap-8 mt-4">
                                 <div className="flex gap-2 items-center bg-[#FF5A3D] text-white px-4 py-2 rounded-lg font-semibold">
                                 <FiSquare className="text-lg" />
                                 <p>{area}</p>
