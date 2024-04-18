e86c33ab462ace97c5aeba0630c35665e31a6171
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-04-18 18:29:52 +0600

    user profile route added in the navbar and bug fixed

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -17,6 +17,7 @@ const Navbar = () => {
     <>
         <li><NavLink to={'/'}>Home</NavLink></li>
         {user && <>
+            <li><NavLink to={'/profile'}>User Profile</NavLink></li>
             <li><NavLink to={'/profileUpdate'}>Update Profile</NavLink></li>
             <li><NavLink to={'/blog'}>Blog</NavLink></li></>
         }
@@ -26,7 +27,7 @@ const Navbar = () => {
         <div className="navbar bg-base-100 mt-6 ">
             <div className="navbar-start">
                 <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost md:hidden lg:hidden">
+                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </div>
                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 lg:z-[1] p-2 shadow bg-base-100 rounded-box w-52 z-10">
@@ -38,7 +39,7 @@ const Navbar = () => {
                     Radiant Residences
                 </Link>
             </div>
-            <div className="navbar-center hidden md:flex lg:flex">
+            <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     {lists}
                 </ul>
